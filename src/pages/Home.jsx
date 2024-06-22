9cb0ffd39a1eb60f81f03e5fdb2a2e409583cafb
Donald Edinam Kobby <76127724+Donald-Edinam@users.noreply.github.com>
2024-06-22 12:09:39 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="mt-20 mx-auto">
-      <h1 className="text-center">Home</h1>
+      <p className="text-center text-red-500">This app is under development, thanks for testing 👩‍💻</p>
+      <h1 className="text-center font-bold">Home</h1>
     </div>
   )
 }
